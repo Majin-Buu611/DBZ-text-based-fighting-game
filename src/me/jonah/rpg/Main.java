a36f9b5be0862c8e7003c743e0bc04655792279f
Majin-Buu611 <72584072+Majin-Buu611@users.noreply.github.com>
2023-01-29 17:57:10 -0500

    created Main.java

---
+++ src/me/jonah/rpg/Main.java
@@ -0,0 +1,44 @@
+package me.jonah.rpg;
+
+import java.util.ArrayList;
+import java.util.List;
+import me.jonah.rpg.characters.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Player player = new Player("Player 1", 100, 100, 20, 10, 1, 0, 20);
+    List<Enemy> enemies = new ArrayList<>();
+    enemies.add(new Zombie("Zombie 1", 50, 50, 15, 5, 1));
+    enemies.add(new Zombie("Zombie 2", 60, 60, 18, 6, 2));
+    enemies.add(new Zombie("Zombie 3", 70, 70, 21, 7, 3));
+    enemies.add(new Zombie("Zombie 4", 80, 80, 24, 8, 4));
+
+    Enemy enemy = chooseEnemy(player, enemies);
+    while (player.isAlive() && enemy.isAlive()) {
+      player.attack(enemy);
+      if (enemy.isAlive()) {
+        enemy.attack(player);
+      }
+      if (!player.isAlive()) {
+        System.out.println("You have been defeated.");
+        break;
+      }
+      if (!enemy.isAlive()) {
+        System.out.println("You have defeated " + enemy.getName() + " and gained " + enemy.getExp() + " exp.");
+        player.gainExp(enemy.getExp());
+        enemy = chooseEnemy(player, enemies);
+      }
+    }
+  }
+
+  public static Enemy chooseEnemy(Player player, List<Enemy> enemies) {
+    List<Enemy> availableEnemies = new ArrayList<>();
+    for (Enemy e : enemies) {
+      if (e.getLevel() <= player.getLevel()) {
+        availableEnemies.add(e);
+      }
+    }
+    int randomIndex = (int) (Math.random() * availableEnemies.size());
+    return availableEnemies.get(randomIndex);
+  }
+}
