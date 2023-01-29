0b7608818d9971725f6ed5dfa967e7f1dbfcd5f4
Majin-Buu611 <72584072+Majin-Buu611@users.noreply.github.com>
2023-01-29 17:51:46 -0500

    updated Enemy.java

--- src/me/jonah/rpg/characters/enemies/Enemy.java
+++ src/me/jonah/rpg/characters/enemies/Enemy.java
@@ -3,15 +3,27 @@ package me.jonah.rpg.characters.enemies;
 import me.jonah.rpg.characters.Character;
 
 public abstract class Enemy extends Character {
-protected int Level;
+protected int minLevel;
 protected int expGivenOnDeath;
-public Enemy(String name, int health, int maxHealth, int attack, int defense, int level, int expGivenOnDeath) {
+public Enemy(String name, int health, int maxHealth, int attack, int defense, int expOnDeath, int expGivenOnDeath) {
 super (name, health, maxHealth, attack, defense);
-this.level = level;
-this.expGivenOnDeath = expGivenOnDeath;
+this.minLevel = minLevel;
+this.expOnDeath = expOnDeath;
+}
+
+public int getMinLevel() {
+return minLevel;
+}
+
+public void setMinLevel(int minLevel) {
+this.minLevel = minLevel;
 }
 
 public int getExpOnDeath() {
-return expGivenOnDeath;
+return expOnDeath;
+}
+
+public void setExpOnDeath(int expOnDeath) {
+this.expOnDeath = expOnDeath;
 }
 }
